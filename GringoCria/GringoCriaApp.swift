49f2402a1cd4926aa2f996dbbfe8b97e64b09d37
João Victor Magno <jvmagnob105@gmail.com>
2026-05-11 14:30:26 -0300

    Initial Commit

---
+++ GringoCria/GringoCriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GringoCriaApp.swift
+//  GringoCria
+//
+//  Created by João Victor Magno on 11/05/26.
+//
+
+import SwiftUI
+
+@main
+struct GringoCriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
